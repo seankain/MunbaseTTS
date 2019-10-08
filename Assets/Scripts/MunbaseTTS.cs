2cb2dda263201a428cef7d4be8c16482eb11e8a3
unknown <sean.kain.geo@gmail.com>
2019-10-07 21:16:56 -0400

    Error with Fonix dlls in Unity due to being compiled for 32bit

---
+++ Assets/Scripts/MunbaseTTS.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SharpTalk;
+
+public class MunbaseTTS : MonoBehaviour
+{
+    private InputField textInput;
+    private FonixTalkEngine tts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textInput = GetComponent<InputField>();
+        tts = new FonixTalkEngine();
+        //using (var tts = new FonixTalkEngine())
+        //{
+        //    string msg;
+        //    while ((msg = Console.ReadLine()) != "exit")
+        //    {
+        //        tts.Speak(msg);
+        //    }
+        //}
+        //phoneme<duration,pitch>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            tts.Speak(textInput.text);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        tts.Dispose();
+    }
+
+}
